fca7c5ac38e18f8df20e37e109a516ecfda4352a
MynorEscobar <mynorescobarefpem@gmail.com>
2024-10-07 14:43:53 -0600

    actualizacion

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,5 +9,5 @@ $routes->get('/', 'Home::index');
 $routes->get('/api/planes','Planes::index');
 $routes->get('/api/planes/(:num)','Planes::show/$1');
 $routes->post('/api/planes','Planes::create');
-$routes->patch('/api/planes/(:num)','Planes::update/$1');
+$routes->put('/api/planes/(:num)','Planes::update/$1');
 $routes->delete('/api/planes/(:num)','Planes::delete/$1');
